478272b7c85570f19887243989634e0459a3202d
aungmyomyat220 <aungmyomyat874@gmail.com>
2024-01-26 14:43:39 +0630

    env val add

--- hooks/updatePostHook.js
+++ hooks/updatePostHook.js
@@ -1,13 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
-
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const updatePostHook = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const queryClient = useQueryClient();
     // eslint-disable-next-line react-hooks/rules-of-hooks
     return useMutation(
         async ({Id,updateData}) => {
-          return await axios.patch(`/posts/${Id}`, updateData);
+          return await axios.patch(`${API_BASE_URL}/posts/${Id}`, updateData,{
+              headers: {
+                  "API_KEY" : apiKey
+              }
+          });
         },
         {
             onSuccess: () => {
--- hooks/updateUserHook.js
+++ hooks/updateUserHook.js
@@ -1,12 +1,18 @@
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import axios from "axios";
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const updateUserHook = () => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const queryClient = useQueryClient()
     // eslint-disable-next-line react-hooks/rules-of-hooks
     return useMutation(
       async ({Id,updateData}) => {
-          return await axios.put(`https://my-blog-beta-green.vercel.app/users/${Id}`, updateData);
+          return await axios.put(`${API_BASE_URL}/${Id}`, updateData,{
+            headers: {
+              "API_KEY" : apiKey
+            }
+          });
       },
       {
           onSuccess: () => {
